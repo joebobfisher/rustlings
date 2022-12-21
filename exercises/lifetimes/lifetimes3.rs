6c0cb1de72b6154d489ccd0dd08ff0ed727decd9
Joe Fisher <fisher.joe@gmail.com>
2022-12-20 19:17:14 -0600

    Lifetime exercises done.

--- exercises/lifetimes/lifetimes3.rs
+++ exercises/lifetimes/lifetimes3.rs
@@ -4,11 +4,9 @@
 //
 // Execute `rustlings hint lifetimes3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
-struct Book {
-    author: &str,
-    title: &str,
+struct Book<'lifetime> {
+    author: &'lifetime str,
+    title: &'lifetime str,
 }
 
 fn main() {
