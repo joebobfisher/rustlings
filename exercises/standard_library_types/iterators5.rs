d1ed3118fe11339f96b6916d68eaf883ab6f04cd
Joe Fisher <fisher.joe@gmail.com>
2022-12-25 13:55:14 -0600

    Final iterator exercise.

--- exercises/standard_library_types/iterators5.rs
+++ exercises/standard_library_types/iterators5.rs
@@ -1,3 +1,4 @@
+
 // iterators5.rs
 // Let's define a simple model to track Rustlings exercise progress. Progress
 // will be modelled using a hash map. The name of the exercise is the key and
@@ -9,8 +10,6 @@
 // Execute `rustlings hint iterators5` or use the `hint` watch subcommand for a hint.
 //
 // Make the code compile and the tests pass.
-
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -34,7 +33,7 @@ fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
 fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
     // map is a hashmap with String keys and Progress values.
     // map = { "variables1": Complete, "from_str": None, ... }
-    todo!();
+    map.iter().filter(|x| *x.1 == value).count()
 }
 
 fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
@@ -53,7 +52,11 @@ fn count_collection_iterator(collection: &[HashMap<String, Progress>], value: Pr
     // collection is a slice of hashmaps.
     // collection = [{ "variables1": Complete, "from_str": None, ... },
     //     { "variables2": Complete, ... }, ... ]
-    todo!();
+    collection
+        .iter()
+        .map(|x|
+            x.iter().filter(|x| *x.1 == value).count())
+        .fold(0, |acc, x| acc + x)
 }
 
 #[cfg(test)]
